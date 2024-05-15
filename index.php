09032621f69a9acb6d5fa5a4067e50365ed8b6c3
nafi-ullah <shafinnafiullah@gmail.com>
2024-05-15 21:33:01 +0600

    user with jwt

---
+++ index.php
@@ -0,0 +1,145 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+header("Access-Control-Allow-Methods: *");
+
+include 'DbConnect.php';
+include 'vendor/autoload.php'; // Composer autoload
+
+use Firebase\JWT\JWT;
+
+$objDb = new DbConnect;
+$conn = $objDb->connect();
+
+// $secret_key = "YOUR_SECRET_KEY";
+// $issuer_claim = "THE_ISSUER"; // this can be the servername
+
+$method = $_SERVER['REQUEST_METHOD'];
+switch($method) {
+    case "GET":
+        $sql = "SELECT userid, username, fullname, profilepic, email, bio, countrylist FROM users";
+        $path = explode('/', $_SERVER['REQUEST_URI']);
+        if(isset($path[3]) && is_numeric($path[3])) {
+            $sql .= " WHERE userid = :userid";
+            $stmt = $conn->prepare($sql);
+            $stmt->bindParam(':userid', $path[3]);
+            $stmt->execute();
+            $users = $stmt->fetch(PDO::FETCH_ASSOC);
+        } else {
+            $stmt = $conn->prepare($sql);
+            $stmt->execute();
+            $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        }
+
+        echo json_encode($users);
+        break;
+
+    case "POST":
+        $data = json_decode( file_get_contents('php://input') );
+        if ($data->action == "login") {
+            $email = $data->email;
+            $password = $data->password;
+
+            $sql = "SELECT * FROM users WHERE email = :email";
+            $stmt = $conn->prepare($sql);
+            $stmt->bindParam(':email', $email);
+            $stmt->execute();
+            $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            if ($user) {
+                if (password_verify($password, $user['password'])) {
+                    $secret_key = 'tourist_secret_key';
+
+            $jwt_payload = [
+                'iss' => 'naficoder',
+                'iat' => time(),
+                'exp' => strtotime("+1 hour"),
+                'userid' => $user['userid'],
+                'username' => $user['username']
+                // You can add more data to the payload if needed
+            ];
+           
+
+            $jwt_token = JWT::encode($jwt_payload, $secret_key, 'HS256');
+                    echo json_encode(
+                        array(
+                            "message" => "Successful login.",
+                            'user' => $user,
+                            "jwt" => $jwt_token,
+                          
+                        ));
+                } else {
+                    http_response_code(401);
+                    echo json_encode(array("message" => "Login failed. Incorrect credentials."));
+                }
+            } else {
+                http_response_code(401);
+                echo json_encode(array("message" => "Login failed. User not found."));
+            }
+        } elseif ($data->action == "register") {
+            $username = $data->username;
+            $fullname = $data->fullname;
+            $email = $data->email;
+            $password = password_hash($data->password, PASSWORD_BCRYPT); // encrypt password
+            // Other fields
+
+            $sql = "INSERT INTO users(username, fullname, email, password) VALUES(:username, :fullname, :email, :password)";
+            $stmt = $conn->prepare($sql);
+            $stmt->bindParam(':username', $username);
+            $stmt->bindParam(':fullname', $fullname);
+            $stmt->bindParam(':email', $email);
+            $stmt->bindParam(':password', $password);
+
+            if($stmt->execute()) {
+                $response = ['status' => 1, 'message' => 'User registered successfully.'];
+            } else {
+                $response = ['status' => 0, 'message' => 'Failed to register user.'];
+            }
+            echo json_encode($response);
+        } else {
+            http_response_code(400);
+            echo json_encode(array("message" => "Invalid action."));
+        }
+        break;
+
+    case "PUT":
+        // Update operation
+        $user = json_decode( file_get_contents('php://input') );
+        $sql = "UPDATE users SET username = :username, fullname = :fullname, profilepic = :profilepic, email = :email, password = :password, bio = :bio, countrylist = :countrylist WHERE userid = :userid";
+        $stmt = $conn->prepare($sql);
+        $stmt->bindParam(':userid', $user->userid);
+        $stmt->bindParam(':username', $user->username);
+        $stmt->bindParam(':fullname', $user->fullname);
+        $stmt->bindParam(':profilepic', $user->profilepic);
+        $stmt->bindParam(':email', $user->email);
+        $stmt->bindParam(':password', $user->password);
+        $stmt->bindParam(':bio', $user->bio);
+        $stmt->bindParam(':countrylist', $user->countrylist);
+       
+
+        if($stmt->execute()) {
+            $response = ['status' => 1, 'message' => 'Record updated successfully.'];
+        } else {
+            $response = ['status' => 0, 'message' => 'Failed to update record.'];
+        }
+        echo json_encode($response);
+        break;
+
+    case "DELETE":
+        // Delete operation
+        $sql = "DELETE FROM users WHERE userid = :userid";
+        $path = explode('/', $_SERVER['REQUEST_URI']);
+
+        $stmt = $conn->prepare($sql);
+        $stmt->bindParam(':userid', $path[3]);
+
+        if($stmt->execute()) {
+            $response = ['status' => 1, 'message' => 'Record deleted successfully.'];
+        } else {
+            $response = ['status' => 0, 'message' => 'Failed to delete record.'];
+        }
+        echo json_encode($response);
+        break;
+}
